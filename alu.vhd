ff425aaa0d44fed1144f354ec1ad26552cc86433
Ziad-Beshlawy <61118586+Ziad-Beshlawy@users.noreply.github.com>
2020-02-21 13:58:47 +0200

    Add files via upload

--- alu.vhd
+++ alu.vhd
@@ -1,5 +1,4 @@
 -- only zero flag.
--- "std_logic" inputs have to be cast to "signed".
 
 Library ieee;
 use ieee.std_logic_1164.all;
@@ -7,8 +6,8 @@ use ieee.numeric_std.all;
 
 entity alu is
 	port(
-		a, b    : in  signed(15 downto 0); -- inputs
-		c       : out signed(15 downto 0); -- result
+		a, b    : in  std_logic_vector(15 downto 0); -- inputs
+		c       : out std_logic_vector(15 downto 0); -- result
 		control : in  std_logic_vector(2 downto 0); -- from "FUNC" segment of R type instructions or a decoder for other instructions.
 		zf      : out std_logic                      -- flags
 		);
@@ -29,16 +28,16 @@ signal result : signed(15 downto 0); -- used to get flags
 
 begin
 	with control select
-	result <= a + b   when "000",
-		      a - b   when "001",
-		      a and b when "010",
-		      a or b  when "011",
-		      a nor b when "100",
-		      a xor b when "101",
-		      a sll 1 when "110",
-		      a srl 1 when "111",
+	result <= signed(a) + signed(b)   when "000",
+		      signed(a) - signed(b)   when "001",
+		      signed(a) and signed(b) when "010",
+		      signed(a) or signed(b)  when "011",
+		      signed(a) nor signed(b) when "100",
+		      signed(a) xor signed(b) when "101",
+		      signed(a) sll 1 when "110",
+		      signed(a) srl 1 when "111",
 			  (others => '0') when others;
-	c <= result;		  
+	c <= std_logic_vector(result);		  
 			  
 	zf <= '1' when (result = x"0000") else
 		  '0';
